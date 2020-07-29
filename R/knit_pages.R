60e4f2e5a002782d2cf18dddc0b3d0149733cfea
Bryan Blanc <bpb824@users.noreply.github.com>
2020-07-29 10:36:07 -0700

    Folding in R setup content

--- R/knit_pages.R
+++ R/knit_pages.R
@@ -1,7 +1,10 @@
 require(knitr)
 require(rmarkdown)
 
+#Home page
 rmarkdown::render('markdown/index_gen.Rmd',output_format = 'all','../index.html')
+
+#Course topics
 rmarkdown::render('markdown/shiny_1.Rmd',output_format = 'all','../topics/shiny_1.html')
 rmarkdown::render('markdown/shiny_2.Rmd',output_format = 'all','../topics/shiny_2.html')
 rmarkdown::render('markdown/shiny_3.Rmd',output_format = 'all','../topics/shiny_3.html')
@@ -12,3 +15,6 @@ rmarkdown::render('markdown/census.Rmd',output_format = 'all','../topics/census.
 rmarkdown::render('markdown/gtfs_data.Rmd',output_format = 'all','../topics/gtfs_data.html')
 rmarkdown::render('markdown/rmarkdown.Rmd',output_format = 'all','../topics/rmarkdown.html')
 rmarkdown::render('markdown/misc_questions.Rmd',output_format = 'all','../topics/misc_questions.html')
+
+#Other topics
+rmarkdown::render('markdown/r_setup_nn.Rmd',output_format = 'all','../other_topics/r_setup_nn.html')
